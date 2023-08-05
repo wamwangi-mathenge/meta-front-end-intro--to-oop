fd225a3ac06473b05614bb9e8c1c13349bc0acf4
Brian <wamwangimathenge@gmail.com>
2023-08-05 11:54:49 +0300

    Calculate the total cost using the OOP paradigm
    This commit uses the Object Oriented Programming paradigm to calculate
    the total cost of buying the pair of shoes

---
+++ oop.js
@@ -0,0 +1,12 @@
+// Write a program that calculates the total cost of buying a pair of shoes.
+
+var purchase1 = {
+    shoes: 100,
+    stateTax: 1.2,
+    totalPrice: function() {
+        var calculation = purchase1.shoes * purchase1.stateTax;
+        console.log("Total price:", calculation)
+    }
+}
+
+purchase1.totalPrice();
